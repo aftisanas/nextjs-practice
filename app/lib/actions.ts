0df325014cde0fa4274ecc0638d6dc6d21e5e869
Anas AFTIS <afanas650@gmail.com>
2024-01-07 16:12:55 +0100

    commit changes

---
+++ app/lib/actions.ts
@@ -0,0 +1,119 @@
+'use server';
+
+import { string, z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+// create new invoice
+// Use Zod to update the expected types
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+
+    const customerId = formData.get("customerId");
+    const amount = parseFloat(formData.get("amount") as string);
+    const status = formData.get("status");
+
+    try {
+
+        CreateInvoice.parse({
+            customerId,
+            amount,
+            status,
+        });
+
+    } catch (validationError) {
+
+        return { message: 'Validation Error: Invalid input.', error: validationError };
+
+    }
+
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split("T")[0];
+
+    try {
+
+        await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId?.toString()}, ${amountInCents}, ${status?.toString()}, ${date})
+        `;
+    
+    } catch (error) {
+    
+        return { message: "Database Error: Failed to Create Invoice.", error };
+    
+    }
+    
+    revalidatePath("/dashboard/invoices");
+    redirect("/dashboard/invoices");
+
+}
+
+// update an invoice
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+    
+    const customerId = formData.get("customerId");
+    const amount = parseFloat(formData.get("amount") as string);
+    const status = formData.get("status");
+
+    try {
+        
+        UpdateInvoice.parse({
+            customerId,
+            amount,
+            status,
+        });
+
+    } catch (validationError) {
+
+        return { message: 'Validation Error: Invalid input.', error: validationError };
+
+    }
+
+    const amountInCents = amount * 100;
+
+    try {
+        
+        await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId?.toString()}, amount = ${amountInCents}, status = ${status?.toString()}
+            WHERE id = ${id}
+        `;
+        
+    } catch (error) {
+        
+        return { message: 'Database Error: Failed to Update Invoice.', error: error}
+        
+    }
+    
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+
+}
+
+// delete invoice
+export async function deleteInvoice(id: string) {
+    try {
+        
+        await sql`DELETE FROM invoices WHERE id = ${ id }`;
+
+        revalidatePath('/dashboard/invoices');
+        
+    } catch (error) {
+        
+        return { message: 'Database Error: Failed to Delete Invoice.', error: error}
+
+    }
+}
