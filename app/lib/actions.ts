cfa5b21579520e199aabba53495b453039e1adfc
Anas AFTIS <afanas650@gmail.com>
2024-01-08 18:01:48 +0100

    fixing redirect

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -20,12 +20,10 @@ export async function authenticate (
     formData: FormData
 ) {
     try {
-        
-        await signIn( 'credentials', formData);
-
+        await signIn('credentials', formData);
     } catch (error) {
-        if ( error instanceof AuthError) {
-            switch ( error.type ) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
                 case 'CredentialsSignin':
                     return 'Invalid credentials.';
                 default:
